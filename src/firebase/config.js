7f66348740761aab598ac2821e0b0a048cf5cf37
Diego P. Bernal <diego.bernal17@gmail.com>
2019-04-21 11:01:33 -0500

    Add favicon and title

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  apiKey: "AIzaSyDdgmu66Aqt0SUsAQ0VUB9Y2prmU1bThoc",
-  authDomain: "dbernalpage.firebaseapp.com",
-  databaseURL: "https://dbernalpage.firebaseio.com",
-  projectId: "dbernalpage",
-  storageBucket: "dbernalpage.appspot.com",
-  messagingSenderId: "849739939108"
-};
-export { config };
